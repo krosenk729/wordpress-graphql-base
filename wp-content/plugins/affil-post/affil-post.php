3b3285634bb9ddc0e1b1e3731b39656a94675f5c
krosenk729 <krosenk729@gmail.com>
2019-08-18 15:05:01 -0400

    adding cors

--- wp-content/plugins/affil-post/affil-post.php
+++ wp-content/plugins/affil-post/affil-post.php
@@ -71,4 +71,9 @@ function custom_post_type()
 }
 add_action( 'init', 'custom_post_type', 0 );
 
+function add_cors_http_header(){
+	header("Access-Control-Allow-Origin: *");
+}
+add_action('init','add_cors_http_header');
+
 ?>
